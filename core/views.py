4eebb5077b0b8ae8b8cfead3ca6a886471fd069d
Saurabh Biswas <visnuk252@gmail.com>
2023-04-21 21:33:25 +0530

    made follow button functional

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 
 
 @login_required(login_url='signin')
@@ -52,6 +52,25 @@ def like_post(request):
         return redirect('/')
 
 
+@login_required(login_url='signin')
+def follow(request):
+
+    if request.method == 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/' + user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/' + user)
+    else:
+        return redirect('/')
+
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -94,11 +113,20 @@ def profile(request, pk):
     user_posts = Post.objects.filter(user=pk)
     user_post_length = len(user_posts)
 
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follower, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+
     context = {
         'user_object': user_object,
         'user_profile': user_profile,
         'user_posts': user_posts,
         'user_post_length': user_post_length,
+        'button_text': button_text,
 
     }
     return render(request, 'profile.html', context)
