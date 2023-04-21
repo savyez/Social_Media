f929f0ca9ea16886546ccb5aca0788e2c87e3d3f
Saurabh Biswas <visnuk252@gmail.com>
2023-04-21 12:31:41 +0530

    User Log Out

--- core/views.py
+++ core/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -53,6 +55,12 @@ def signin(request):
             return redirect('/')
         else:
             messages.info(request, 'Wrong Info given.')
-            return redirect('/signin')
+            return redirect('signin')
     else:
         return render(request, 'signin.html')
+
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
