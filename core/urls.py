6d47a7a0fbf38fe576271c477a489e05d0c87427
Saurabh Biswas <visnuk252@gmail.com>
2023-04-21 19:54:52 +0530

    User Interaction - Profile Page

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout', views.logout, name='logout'),
     path('settings', views.settings, name='settings'),
     path('like-post', views.like_post, name='like-post'),
+    path('profile/<str:pk>', views.profile, name='profile'),
     path('upload', views.upload, name='upload')
 
 ]
